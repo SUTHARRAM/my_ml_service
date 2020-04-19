08ad56b80feae1cbf6514b97ba43d566eb5e85b7
SUTHARRAM <rls42847@gmail.com>
2020-04-19 10:49:56 +0530

    Views and MOdels are added.

---
+++ backend/server/apps/endpoints/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(Endpoint)
+admin.site.register(MLAlgorithm)
+admin.site.register(MLAlgorithmStatus)
+admin.site.register(MLRequest)
+
